b4555238cd5def138f212cfcbb26a1e70e5b9745
case5641git <t.hiroto.job@gmail.com>
2024-08-09 11:13:35 +0900

    feature:feature/BE_006

--- api/app/tests/Feature/LoginTest.php
+++ api/app/tests/Feature/LoginTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use App\Models\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\Response;
 
 class LoginTest extends TestCase
 {
@@ -85,5 +86,58 @@ class LoginTest extends TestCase
         $this->assertGuest();
         $response->assertRedirect('/api/login');
         
+    }
+
+
+    /**
+     * 新規登録処理　バリデーションエラー
+     * @return void
+     */
+    public function test_registrate_validate_error():void
+    {
+        $response = $this->post('/api/register', [
+            'name' => '',
+            'email' => 'invalid-email',
+            'password' => '',
+        ]);
+        $response->assertSessionHasErrors([
+            'name' => '名前 を入力してください。',
+            'email' => '正しいメールアドレスを入力してください。',
+            'password' => 'パスワード を入力してください。',
+        ]);
+    }
+
+    /**
+     * 新規登録処理　登録済みメールアドレス
+     * @return void
+     */
+    public function test_registrate_email_exists():void
+    {
+        User::factory()->create([
+            'email' => 'test@example.com',
+        ]);
+        $response = $this->post('/api/register', [
+            'name' => 'test',
+            'email' => 'test@example.com',
+            'password' => 'password',
+        ]);
+        $response->assertSessionHasErrors([
+            'email' => 'この メールアドレス は既に使用されています。'
+        ]);
+    }
+
+    /**
+     * 新規登録処理
+     * @return void
+     */
+    public function test_registrate_success():void
+    {
+        $response = $this->post('/api/register', [
+            'name' => 'test',
+            'email' => 'test@example.com',
+            'password' => 'password',
+        ]);
+        $response->assertStatus(Response::HTTP_FOUND);
+        $response->assertRedirect('/api/login');
     }
 }
