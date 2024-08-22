a74a14460c313ad3bec91c362436948d522cdccc
case5641git <t.hiroto.job@gmail.com>
2024-08-22 09:07:02 +0900

    feature:feature/FE_-49

--- api/app/app/Http/Controllers/AuthController.php
+++ api/app/app/Http/Controllers/AuthController.php
@@ -2,33 +2,58 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
 use Illuminate\Http\Request;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
+use App\Http\Resources\UserResource;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     /**
      * ログイン処理
+     * @param Request $request
+     * @return JsonResponse
      */
-    public function login(Request $request): RedirectResponse
-    {
-        $credentials = $request->validate([
+    // public function login(Request $request): JsonResponse
+    // {
+    //     $credentials = $request->validate([
+    //         'email' => 'required|email',
+    //         'password' => 'required',
+    //     ]);
+
+    //     if (Auth::attempt($credentials)) {
+    //         $request->session()->regenerate();
+    //         return response()->json(['token' => $request->user()->createToken('API Token')->plainTextToken]);
+    //     }
+    //     return response()->json(['message'=>'ログインに失敗しました。'], Response::HTTP_UNAUTHORIZED);
+    // }
+    public function login(Request $request) {
+        $data = $request->validated([
             'email' => 'required|email',
             'password' => 'required',
         ]);
 
-        if (Auth::attempt($credentials)) {
-            $request->session()->regenerate();
-            return redirect()->intended('/');
+        $user = User::where('email', $data['email'])->first();
+
+        if (!$user || !Hash::check($data['password'], $user->password)) {
+            return response()->json([
+                'message' => 'メールアドレスかパスワードが正しくありません'
+            ], 401);
         }
-        return back()->withErrors([
-            'email' => '正しいメールアドレスを入力してください。',
-            'password' => '正しいパスワードを入力してください。',
-        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        $cookie = cookie('token', $token, 60 * 24);
+
+        return response()->json([
+            'user' => new UserResource($user),
+        ])->withCookie($cookie);
     }
 
     /**
@@ -61,7 +86,7 @@ class AuthController extends Controller
         } catch (\Exception $e) {
             return response()->json(['message' => 'ユーザー登録に失敗しました。'], Response::HTTP_INTERNAL_SERVER_ERROR);
         }        
-        return redirect()->route('login');
+        return response()->json(['message' => 'ユーザー登録が完了しました。'], Response::HTTP_CREATED);
     }
 
     /**
